8b52dd5541a7be18c08ac8e713828c4913dcedc2
Armin Sander <armin@replicator.org>
2014-02-21 23:34:10 +0100

    add test to verify that newly referenced orphanes are not invalidated

--- Bricks.Tests/GCTests.fs
+++ Bricks.Tests/GCTests.fs
@@ -40,3 +40,41 @@ type GCTests() =
         }
 
         p Program.empty |> ignore
+
+    [<Test>]
+    member this.GCDoesNotInvalidateOrphanThatWasReused() =
+        let a = brick { return 3 }
+
+        let b = brick {
+            let! a = a
+            return a * 3
+           }
+
+        let c = brick {
+            let! b = b
+            return b * 4
+        }
+
+        let overwriteB = transaction {
+                write b 2
+            }
+
+        let resetB = transaction { reset b }
+
+        let p = program {
+            let! c1 = c
+            c1 |> should equal 36
+            apply overwriteB
+            let! c2 = c
+            c2 |> should equal 8
+            let! va1 = valueOf a
+            va1 |> should equal (Some 3)
+            apply resetB
+            let! c3 = c
+            c3 |> should equal 36
+            collect
+            let! va2 = valueOf a
+            va2 |> should equal (Some 3)
+        }
+
+        p Program.empty |> ignore
