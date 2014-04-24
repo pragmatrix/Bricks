2b49884c5995202aa4f5d6f05ca9a4d929388725
Armin Sander <armin@replicator.org>
2014-04-24 23:40:03 +0200

    modularized bricks

---
+++ Bricks/BrickDefs.fs
@@ -0,0 +1,40 @@
+﻿module BrickDefs
+
+open System.Collections.Immutable
+open System.Collections.Generic
+
+module List =
+    let flatten l = List.collect id l
+
+module Seq = 
+    let flatten l = Seq.collect id l
+
+type HashSet = ImmutableHashSet
+type HashSet<'k> = ImmutableHashSet<'k>
+type 'k set = HashSet<'k>
+
+type HashMap = ImmutableDictionary
+type HashMap<'k, 'v> = ImmutableDictionary<'k, 'v>
+
+type ImmutableHashSet<'v> with
+    member this.has v = this.Contains v
+
+module map =
+    let get (m:HashMap<'k, 'v>) k = 
+        let has, v = m.TryGetValue k
+        if has then Some v else None
+    let has (m:HashMap<'k, 'v>) k = m.ContainsKey k
+
+type ImmutableDictionary<'k, 'v> with
+    member this.get k = map.get this k
+    member this.has k = map.has this k
+
+type ImmutableDictionary with
+    static member fromSeq seq = 
+        ImmutableDictionary.CreateRange(Seq.map (fun (k, v) -> KeyValuePair(k, v)) seq)
+
+let inline curry f a b = f (a, b)
+let inline uncurry f (a, b) = f a b
+let inline private (|?) (a: 'a option) b = if a.IsSome then a.Value else b
+
+let isSame a b = obj.ReferenceEquals(a, b)
---
+++ Bricks/BrickSet.fs
@@ -0,0 +1,25 @@
+﻿module BrickSet
+
+open BrickDefs
+
+module bset = 
+    let empty<'e> = HashSet<'e>.Empty
+    let ofSeq = HashSet.CreateRange
+
+    type Change<'v> =
+        | Added of 'v
+        | Removed of 'v
+
+    let diff (s1: 'e set) (s2 : 'e set) = 
+        // tbd: combine finding added / modified
+        let added = 
+            s2
+            |> Seq.filter (s1.has >> not)
+            |> Seq.map Added
+
+        let removed = 
+            s1 
+            |> Seq.filter (s2.has >> not)
+            |> Seq.map Removed
+
+        [removed; added] |> Seq.flatten
